108558b65f6a4f50273c3091a98aa305f9249431
Yanis Plevokas <whitenesscat@gmail.com>
2020-11-11 22:55:14 +0300

    first

---
+++ 05/main.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include "serializator.hpp"
+#include <sstream>   
+
+#define checkEqual(x, y) do { if ((x) != (y)) { std::cout << "at line " << __LINE__ << ": " << (x) << " != " << (y) << '\n'; }; } while(0)
+#define checkTrue(cond) do { if (!(cond)) std::cout << "at line " << __LINE__ << ": " << #cond << '\n'; } while(0)
+
+struct Data
+{
+    uint64_t a;
+    bool b;
+    uint64_t c;
+
+    template <class Serializer>
+    Error serialize(Serializer& serializer)
+    {
+        return serializer(a, b, c);
+    }
+    template <class Deserializer>
+    Error deserialize(Deserializer& deserializer)
+    {
+        return deserializer(a, b, c);
+    }
+};
+
+
+
+void FirstTest()
+{
+    Data x { 1, true, 2 };
+    std::stringstream s1;
+    Serializer serializer(s1);
+    checkTrue(serializer.save(x) == Error::NoError);
+}
+
+void SecondTest()
+{
+    Data y { 0, false, 0 };
+    std::stringstream s1;
+    Serializer serializer(s1);
+    Deserializer d1(s1);
+    checkTrue(d1.load(y) == Error::NoError);
+}
+
+void ThirdTest()
+{
+    Data x { 1, true, 2 };
+
+    std::stringstream s1;
+
+    Serializer serializer(s1);
+
+    Data y { 0, false, 0 };
+
+    Deserializer d1(s1);
+    checkEqual(x.a, y.a);
+    checkEqual(x.b, y.b);
+    checkEqual(x.c, y.c);
+}
+
+void FourthTest()
+{
+    Data y { 0, false, 0 };
+    auto s2 = std::stringstream("");
+    Deserializer d2(s2);
+    checkTrue(d2.load(y) == Error::CorruptedArchive);
+
+    auto s3 = std::stringstream("1 2 3");
+    Deserializer d3(s3);
+    checkTrue(d3.load(y) == Error::CorruptedArchive);
+
+    auto s4 = std::stringstream("1 true -3");
+    Deserializer d4(s4);
+    checkTrue(d4.load(y) == Error::CorruptedArchive);
+
+    auto s5 = std::stringstream("false 1");
+    Deserializer d5(s5);
+    checkTrue(d5.load(y) == Error::CorruptedArchive);
+
+}
+void FifthTest()
+{
+    Data y { 0, true, 0 };
+
+    auto s6 = std::stringstream("100 false 500");
+    Deserializer d6(s6);
+    checkTrue(d6.load(y) == Error::NoError);
+
+    checkEqual(y.a, 100);
+    checkEqual(y.b, false);
+    checkEqual(y.c, 500);
+}
+
+
+
+int main()
+{   
+    try
+    {
+        FifthTest();
+        SecondTest();
+        ThirdTest();
+        FourthTest();
+        FifthTest();
+    }
+    catch(...)
+    {
+        std::cout << "Everything is awful\n";
+    }
+    std::cout << "Everything is not so bad\n";
+
+    return 0;
+}
---
+++ 05/serializator.hpp
@@ -0,0 +1,133 @@
+#pragma once
+#include <iostream>
+
+enum class Error
+{
+    NoError,
+    CorruptedArchive
+};
+
+class Serializer
+{
+    static constexpr char Separator = ' ';
+public:
+    explicit Serializer(std::ostream& out) : out_(out) {}
+
+    template <class T>
+    Error save(T& object)
+    {
+        return object.serialize(*this);
+    }
+
+    template <class... ArgsT>
+    Error operator()(ArgsT&&... args)
+    {
+        return process(std::forward<ArgsT>(args)...);
+    }
+    
+private:
+	std::ostream& out_;
+
+	template <class T, class ... ArgsT>
+	Error process(T&& object, ArgsT&&... args)
+	{
+		handler(object);
+		return process(std::forward<ArgsT>(args)...);
+	}
+	template <class T>
+	Error process(T&& object)
+	{
+		handler(object);
+		return Error::NoError;
+	}
+	void handler(bool obj)
+	{
+		out_ << (obj ? "true" : "false") << Separator;
+	}
+	void handler(uint64_t obj)
+	{
+		out_ << obj << Separator;
+	}
+
+	
+
+
+};
+
+class Deserializer
+{
+    static constexpr char Separator = ' ';
+public:
+    explicit Deserializer(std::istream& in) : in_(in) {}
+
+    template <class T>
+    Error load(T& object)
+    {
+        return object.deserialize(*this);
+    }
+
+    template <class T, class... ArgsT>
+    Error operator()(T&& object, ArgsT&&... args)
+    {
+        return process(std::forward<ArgsT>(args)...);
+    }
+    
+private:
+
+	std::istream& in_;
+
+	template <class T, class... ArgsT>
+    Error process(T&& object, ArgsT&&... args) 
+    {
+        auto res = funct(object);
+        if (res == Error::CorruptedArchive) 
+        {
+            return Error::CorruptedArchive;
+        }
+        return process(std::forward<ArgsT>(args)...);
+    }
+
+    template <class T>
+	Error process(T&& object)
+	{
+		return funct(object);
+	}
+
+
+	Error funct(bool& value)
+	{
+	    std::string text;
+	    in_ >> text;
+	    if (text == "true")
+	        value = true;
+	    else if (text == "false")
+	        value = false;
+	    else
+	        return Error::CorruptedArchive;
+
+	    return Error::NoError;
+	}
+
+	Error funct(uint64_t& a) {
+        std::string text;
+        in_ >> text;
+        try 
+        {
+            if (!isdigit(text[0])) 
+            {
+                return Error::CorruptedArchive;
+            }
+            a = std::stoull(text);
+            return Error::NoError;
+        } 
+        catch (const std::exception &e) 
+        {
+            return Error::CorruptedArchive;
+        }
+    }
+
+
+
+
+
+};
