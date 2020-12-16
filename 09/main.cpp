0ca820bc3d5bfa31527c13f21bf76cf27038970e
Yanis Plevokas <whitenesscat@gmail.com>
2020-12-16 16:08:14 +0300

    first

---
+++ 09/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <cstdint>
+#include <cstddef>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+#include <chrono>
+#include <cstdio>
+#include "sort.h"
+
+using namespace std;
+
+void gen_test(const string &str, const string &teststr, size_t cnt) 
+{
+	ofstream fout(str, ios::binary);
+	ofstream testfout(teststr, ios::binary);
+	for (size_t i = 0; i < cnt; ++i) {
+		uint64_t tmp = rand() + (1llu << 31) * rand();
+		fout.write((char*)&tmp, sizeof(tmp));
+		testfout.write((char*)&tmp, sizeof(tmp));
+	}
+}
+
+void make_test()
+{
+	gen_test("test.bin", "testtmp.bin", 12345678);
+	allSort("test.bin", "out.bin");
+	ifstream fin("testtmp.bin", ios::binary);
+	ifstream fout("out.bin", ios::binary);
+	vector<uint64_t> v;
+	uint64_t tmp;
+	while (fin.read((char*)&tmp, sizeof(tmp))) 
+	{
+		v.push_back(tmp);
+	}
+	sort(v.begin(), v.end());
+	for (size_t i = 0; i < v.size(); ++i) 
+	{
+		if (fout.read((char*)&tmp, sizeof(tmp))) 
+		{
+			if (tmp != v[i]) 
+			{
+				cout << "Sort problem" << endl;
+				exit(-1);
+			}
+		} 
+		else 
+		{
+			cout << "Not full" << endl;
+			exit(-1);
+		}
+	}
+	remove("test.bin");
+	remove("testtmp.bin");
+	remove("out.bin");
+}
+
+int main() 
+{
+
+	try
+	{
+		make_test();
+	}
+	catch (...)
+	{
+		cout << "Problem\n";
+		exit(-1);
+	}
+	cout << "OK" << endl;
+	return 0;
+}
---
+++ 09/sort.h
@@ -0,0 +1,102 @@
+#pragma once
+#include <fstream>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <mutex>
+#include <thread>
+#include <stdexcept>
+
+const size_t maxSize = 4 * 1024 * 1024 / sizeof(uint64_t) - 100;
+
+using namespace std;
+
+void soloSort(ifstream &fin, mutex &f_mutex, size_t &fnum, mutex &n_mutex) 
+{
+    uint64_t *arr = new uint64_t[maxSize]();
+    size_t top;
+    while (true) 
+    {
+        for (top = 0; top < maxSize; top++) 
+        {
+            unique_lock<mutex> unique(f_mutex);
+            if (!fin.read((char *) &arr[top], sizeof(arr[top]))) 
+            {
+                break;
+            }
+        }
+        if (top == 0) 
+        {
+            delete[] arr;
+            return;
+        }
+        size_t tmp_fnum;
+        {
+            lock_guard<mutex> guard(n_mutex);
+            tmp_fnum = fnum++;
+        }
+        sort(arr, arr + top);
+        ofstream fout("sort_" + to_string(tmp_fnum) + ".bin", ios::binary);
+        if (!fout.is_open()) 
+        {
+            throw runtime_error("Problem with file\n");
+        }
+        fout.write((char *) arr, top * sizeof(arr[0]));
+    }
+}
+
+void allSort(const string &ifn, const string &ofn) 
+{
+    ifstream fin(ifn, ios::binary);
+    if (!fin.is_open()) 
+    {
+        throw runtime_error("file can't be opened");
+    }
+    mutex m1, m2;
+    size_t num = 0;
+    thread first_thread(soloSort, ref(fin), ref(m1), ref(num), ref(m2));
+    thread second_thread(soloSort, ref(fin), ref(m1), ref(num), ref(m2));
+    first_thread.join();
+    second_thread.join();
+    priority_queue< pair<uint64_t, size_t>, vector< pair<uint64_t, size_t> >, greater< pair<uint64_t, size_t> > > q;
+    vector<ifstream> f_vec;
+    for (size_t i = 0; i < num; i++) 
+    {
+        f_vec.emplace_back("sort_" + to_string(i) + ".bin", ios::binary);
+        if (!f_vec[f_vec.size() - 1].is_open()) 
+        {
+            throw runtime_error("Problem with file");
+        }
+    }
+    for (size_t i = 0; i < num; i++) 
+    {
+        int64_t cur;
+        f_vec[i].read((char *) &cur, sizeof(cur));
+        q.push({cur, i});
+    }
+    ofstream fout(ofn, ios::binary);
+    if (!fout.is_open())
+    {
+        throw runtime_error("file cannot be open");
+    }
+    while (!q.empty())
+    {
+        fout.write((char *) &q.top(), sizeof(q.top().first));
+        int64_t cur;
+        if (f_vec[q.top().second].read((char *) &cur, sizeof(cur))) 
+        {
+            q.push({cur, q.top().second});
+        }
+        q.pop();
+    }
+    for (size_t i = 0; i < num; i++)
+    {
+        f_vec[i].close();
+        if (remove(("sort_" + to_string(i) + ".bin").c_str()))
+        {
+            throw runtime_error("file cannot be removed\n");
+        }
+    }
+}
