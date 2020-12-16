b3f9e0a7edb69af61ed08fb1de65f93ae4eebabc
Yanis Plevokas <whitenesscat@gmail.com>
2020-12-16 16:10:21 +0300

    style

--- 09/main.cpp
+++ 09/main.cpp
@@ -11,63 +11,50 @@
 
 using namespace std;
 
-void gen_test(const string &str, const string &teststr, size_t cnt) 
-{
-	ofstream fout(str, ios::binary);
-	ofstream testfout(teststr, ios::binary);
-	for (size_t i = 0; i < cnt; ++i) {
-		uint64_t tmp = rand() + (1llu << 31) * rand();
-		fout.write((char*)&tmp, sizeof(tmp));
-		testfout.write((char*)&tmp, sizeof(tmp));
-	}
+void gen_test(const string &str, const string &teststr, size_t cnt) {
+  ofstream fout(str, ios::binary);
+  ofstream testfout(teststr, ios::binary);
+  for (size_t i = 0; i < cnt; ++i) {
+    uint64_t tmp = rand() + (1llu << 31) * rand();
+    fout.write((char *)&tmp, sizeof(tmp));
+    testfout.write((char *)&tmp, sizeof(tmp));
+  }
 }
 
-void make_test()
-{
-	gen_test("test.bin", "testtmp.bin", 12345678);
-	allSort("test.bin", "out.bin");
-	ifstream fin("testtmp.bin", ios::binary);
-	ifstream fout("out.bin", ios::binary);
-	vector<uint64_t> v;
-	uint64_t tmp;
-	while (fin.read((char*)&tmp, sizeof(tmp))) 
-	{
-		v.push_back(tmp);
-	}
-	sort(v.begin(), v.end());
-	for (size_t i = 0; i < v.size(); ++i) 
-	{
-		if (fout.read((char*)&tmp, sizeof(tmp))) 
-		{
-			if (tmp != v[i]) 
-			{
-				cout << "Sort problem" << endl;
-				exit(-1);
-			}
-		} 
-		else 
-		{
-			cout << "Not full" << endl;
-			exit(-1);
-		}
-	}
-	remove("test.bin");
-	remove("testtmp.bin");
-	remove("out.bin");
+void make_test() {
+  gen_test("test.bin", "testtmp.bin", 12345678);
+  allSort("test.bin", "out.bin");
+  ifstream fin("testtmp.bin", ios::binary);
+  ifstream fout("out.bin", ios::binary);
+  vector<uint64_t> v;
+  uint64_t tmp;
+  while (fin.read((char *)&tmp, sizeof(tmp))) {
+    v.push_back(tmp);
+  }
+  sort(v.begin(), v.end());
+  for (size_t i = 0; i < v.size(); ++i) {
+    if (fout.read((char *)&tmp, sizeof(tmp))) {
+      if (tmp != v[i]) {
+        cout << "Sort problem" << endl;
+        exit(-1);
+      }
+    } else {
+      cout << "Not full" << endl;
+      exit(-1);
+    }
+  }
+  remove("test.bin");
+  remove("testtmp.bin");
+  remove("out.bin");
 }
 
-int main() 
-{
-
-	try
-	{
-		make_test();
-	}
-	catch (...)
-	{
-		cout << "Problem\n";
-		exit(-1);
-	}
-	cout << "OK" << endl;
-	return 0;
+int main() {
+  try {
+    make_test();
+  } catch (...) {
+    cout << "Problem\n";
+    exit(-1);
+  }
+  cout << "OK" << endl;
+  return 0;
 }
